2b57c197d702def110b261506dbc3f894409abcf
Edgar J. Gomez <edgarjgp@gmail.com>
2017-04-07 21:14:49 +0200

    Basic scripts for board and player

---
+++ Assets/Scripts/BoardLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardLogic : MonoBehaviour {
+
+    public GameObject circle;
+
+	// Use this for initialization
+	void Start () {
+        GameObject.Instantiate(circle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

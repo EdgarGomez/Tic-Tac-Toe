7d1e998bf2ad1b59e0f625719bb365b359efd0a2
HR <hector.ramser@gmail.com>
2017-04-07 21:48:38 +0200

    My changes

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class PlayerAction : MonoBehaviour {
 
+    public GameObject circle;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        circle = GameObject.FindGameObjectsWithTag("A1")[1];
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(circle.ToString());
+        }
 	}
 }

0398ec9a5ffa18efeb30f36efd88f6f921b96965
Edgar J. Gomez <edgarjgp@gmail.com>
2017-04-08 02:22:42 +0200

    adding a switch between cross and circles. Adding crosses too. Adding some verbose, noob style

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAction : MonoBehaviour {
-    public GameObject circle;
-    public Transform circle2;
-    public float distance = 10f;
-    Vector2 mousePosition, targetPosition;
 
+    // Circle object and transform properties
+    public GameObject circle;
+    public Transform circlePiece;
+
+    // Cross object and transform properties
+    public GameObject cross;
+    public Transform crossPiece;
+
+    // Setting a distance to calculate the position in the visible area
+    // Defining the mousePosition and the spot We want to show the object
+    public float distance = 10f;
+    Vector2 mousePosition, spotPosition;
+
+    // A simple boolean to switch between one kind of piece and the other
+    public bool isCross = true;
+    
     // Use this for initialization
     void Start()
     {
@@ -17,13 +29,37 @@ public class PlayerAction : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        // Setting the mouse position
+        // Setting the spot position in the visible area
         mousePosition   = Input.mousePosition;
-        targetPosition  = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, distance));
-        circle2.position = targetPosition;
+        spotPosition  = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, distance));
 
+        // Setting circle and cross positions
+        circlePiece.position = spotPosition;
+        crossPiece.position = spotPosition;
+
+        // When left click of the mouse is clicked
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(circle2, circle2.transform.position, circle2.transform.rotation);
+            // We check which piece moves
+            if(isCross)
+            {
+                // If its the cross time, set it to false for the next move and create a cross piece
+                isCross = false;
+                createPiece(circlePiece);
+            } else
+            {
+                // If its the cricle time, set it to true for the next move and create a circle piece
+                isCross = true;
+                createPiece(crossPiece);
+            }
+            
         }
+    }
+
+    // Simple function to instantiate a piece
+    void createPiece (Transform piece)
+    {
+        Instantiate(piece, piece.transform.position, piece.transform.rotation);
     }
 }

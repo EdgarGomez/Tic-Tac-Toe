cc41e3aef898960df508332f503e6bb622d0989b
Edgar J. Gomez <edgarjgp@gmail.com>
2017-04-11 21:42:31 +0200

    Changed pieces sprites - Added winning logic - Added changing board piece for cross or circle

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -1,65 +1,110 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerAction : MonoBehaviour {
 
-    // Circle object and transform properties
-    public GameObject circle;
-    public Transform circlePiece;
-
-    // Cross object and transform properties
-    public GameObject cross;
-    public Transform crossPiece;
-
-    // Setting a distance to calculate the position in the visible area
-    // Defining the mousePosition and the spot We want to show the object
-    public float distance = 10f;
-    Vector2 mousePosition, spotPosition;
+    public Sprite circleSprite;
+    public Sprite crossSprite;
+    private GameObject currentBox;
+    Dictionary<string, int> cells = new Dictionary<string, int>();
 
     // A simple boolean to switch between one kind of piece and the other
     public bool isCross = true;
     
     // Use this for initialization
     void Start()
-    {
-
+    {   
+        cells["A1"] = 0;
+        cells["A2"] = 0;
+        cells["A3"] = 0;
+        cells["B1"] = 0;
+        cells["B2"] = 0;
+        cells["B3"] = 0;
+        cells["C1"] = 0;
+        cells["C2"] = 0;
+        cells["C3"] = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Setting the mouse position
-        // Setting the spot position in the visible area
-        mousePosition   = Input.mousePosition;
-        spotPosition  = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, distance));
 
-        // Setting circle and cross positions
-        circlePiece.position = spotPosition;
-        crossPiece.position = spotPosition;
-
+        if(cells["A1"] == 1 && cells["A2"] == 1 && cells["A3"] == 1 ||
+            cells["A1"] == 1 && cells["B1"] == 1 && cells["C1"] == 1 ||
+            cells["A1"] == 1 && cells["B2"] == 1 && cells["C3"] == 1 ||
+            cells["A2"] == 1 && cells["B2"] == 1 && cells["C2"] == 1 ||
+            cells["A3"] == 1 && cells["B3"] == 1 && cells["C3"] == 1 ||
+            cells["A3"] == 1 && cells["B2"] == 1 && cells["C1"] == 1 ||
+            cells["B1"] == 1 && cells["B2"] == 1 && cells["B3"] == 1 ||
+            cells["C1"] == 1 && cells["C2"] == 1 && cells["C3"] == 1)
+        {
+            Debug.Log("Cross Wins");
+            Time.timeScale = 0;
+        } else if (cells["A1"] == 2 && cells["A2"] == 2 && cells["A3"] == 2 ||
+            cells["A1"] == 2 && cells["B1"] == 2 && cells["C1"] == 2 ||
+            cells["A1"] == 2 && cells["B2"] == 2 && cells["C3"] == 2 ||
+            cells["A2"] == 2 && cells["B2"] == 2 && cells["C2"] == 2 ||
+            cells["A3"] == 2 && cells["B3"] == 2 && cells["C3"] == 2 ||
+            cells["A3"] == 2 && cells["B2"] == 2 && cells["C1"] == 2 ||
+            cells["B1"] == 2 && cells["B2"] == 2 && cells["B3"] == 2 ||
+            cells["C1"] == 2 && cells["C2"] == 2 && cells["C3"] == 2)
+        {
+            Debug.Log("Circle Wins");
+            Time.timeScale = 0;
+        }
         // When left click of the mouse is clicked
         if (Input.GetMouseButtonDown(0))
         {
-            // We check which piece moves
-            if(isCross)
-            {
-                // If its the cross time, set it to false for the next move and create a cross piece
-                isCross = false;
-                createPiece(circlePiece);
-            } else
-            {
-                // If its the cricle time, set it to true for the next move and create a circle piece
-                isCross = true;
-                createPiece(crossPiece);
-            }
-            
+                currentBox = getObjectClicked();
+                changeBoardPiece(currentBox);
         }
     }
 
-    // Simple function to instantiate a piece
-    void createPiece (Transform piece)
+    /// <summary>
+    /// Select the object that was clicked.
+    /// </summary>
+    /// <returns>Game object that was selected.</returns>
+    private GameObject getObjectClicked()
     {
-        Instantiate(piece, piece.transform.position, piece.transform.rotation);
+        GameObject result = null;
+
+        Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+
+        if (hit)
+        {
+            Debug.Log(hit.transform.name);
+            result = hit.transform.gameObject;
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Changes the sprite image from a blank board piece to a cross or circle piece depending on the isCross boolean.
+    /// </summary>
+    /// <param name="currentBox"></param>
+    /// <returns>void</returns>
+    void changeBoardPiece (GameObject currentBox)
+    {
+        if (currentBox.transform.gameObject.layer == 8)
+        {
+            if (cells[(string)currentBox.transform.tag] == 0)
+            {
+                if (isCross)
+                {
+                    currentBox.transform.gameObject.GetComponent<SpriteRenderer>().sprite = crossSprite;
+                    cells[(string)currentBox.transform.tag] = 1;
+                }
+                else
+                {
+                    currentBox.transform.gameObject.GetComponent<SpriteRenderer>().sprite = circleSprite;
+                    cells[(string)currentBox.transform.tag] = 2;
+                }
+            } 
+            isCross = !isCross; 
+        }
     }
 }

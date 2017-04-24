d58f43969f2c91e00fe2e7ace373aff73b6a363e
Edgar J. Gomez <edgarjgp@gmail.com>
2017-04-24 19:19:08 +0200

    Added win logic to BoardLogic script - Referencing dictionary

--- Assets/Scripts/BoardLogic.cs
+++ Assets/Scripts/BoardLogic.cs
@@ -7,6 +7,24 @@ public class BoardLogic : MonoBehaviour {
     // A simple boolean to switch between one kind of piece and the other
     public bool isCross = true;
 
+    public Dictionary<string, int> cells = new Dictionary<string, int>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+        cells["A1"] = 0;
+        cells["A2"] = 0;
+        cells["A3"] = 0;
+        cells["B1"] = 0;
+        cells["B2"] = 0;
+        cells["B3"] = 0;
+        cells["C1"] = 0;
+        cells["C2"] = 0;
+        cells["C3"] = 0;
+
+    }
+
     public void setIsCross(bool isCross)
     {
         if(isCross)
@@ -21,6 +39,47 @@ public class BoardLogic : MonoBehaviour {
     public bool getIsCross()
     {
         return this.isCross;
+    }
+
+    /// <summary>
+    /// Method where is checked who wins.
+    /// </summary>
+    /// <returns>Void</returns>
+
+    public void gameOver()
+    {
+        Debug.Log("A1 = " + cells["A1"]);
+        Debug.Log("A2 = " + cells["A2"]);
+        Debug.Log("A3 = " + cells["A3"]);
+        Debug.Log("B1 = " + cells["B1"]);
+        Debug.Log("B2 = " + cells["B2"]);
+        Debug.Log("B3 = " + cells["B3"]);
+        Debug.Log("C1 = " + cells["C1"]);
+        Debug.Log("C2 = " + cells["C2"]);
+        Debug.Log("C3 = " + cells["C3"]);
+
+        if (cells["A1"] == 1 && cells["A2"] == 1 && cells["A3"] == 1 ||
+            cells["A1"] == 1 && cells["B1"] == 1 && cells["C1"] == 1 ||
+            cells["A1"] == 1 && cells["B2"] == 1 && cells["C3"] == 1 ||
+            cells["A2"] == 1 && cells["B2"] == 1 && cells["C2"] == 1 ||
+            cells["A3"] == 1 && cells["B3"] == 1 && cells["C3"] == 1 ||
+            cells["A3"] == 1 && cells["B2"] == 1 && cells["C1"] == 1 ||
+            cells["B1"] == 1 && cells["B2"] == 1 && cells["B3"] == 1 ||
+            cells["C1"] == 1 && cells["C2"] == 1 && cells["C3"] == 1)
+        {
+            Debug.Log("Cross Wins");
+        }
+        else if (cells["A1"] == 2 && cells["A2"] == 2 && cells["A3"] == 2 ||
+          cells["A1"] == 2 && cells["B1"] == 2 && cells["C1"] == 2 ||
+          cells["A1"] == 2 && cells["B2"] == 2 && cells["C3"] == 2 ||
+          cells["A2"] == 2 && cells["B2"] == 2 && cells["C2"] == 2 ||
+          cells["A3"] == 2 && cells["B3"] == 2 && cells["C3"] == 2 ||
+          cells["A3"] == 2 && cells["B2"] == 2 && cells["C1"] == 2 ||
+          cells["B1"] == 2 && cells["B2"] == 2 && cells["B3"] == 2 ||
+          cells["C1"] == 2 && cells["C2"] == 2 && cells["C3"] == 2)
+        {
+            Debug.Log("Circle Wins");
+        }
     }
 
 
--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -11,73 +11,20 @@ public class PlayerAction : MonoBehaviour {
 
     public GameObject SelectCircle;
     public GameObject SelectCross;
-
-    Dictionary<string, int> cells = new Dictionary<string, int>();
     
     // Use this for initialization
     void Start()
     {
 
-        cells["A1"] = 0;
-        cells["A2"] = 0;
-        cells["A3"] = 0;
-        cells["B1"] = 0;
-        cells["B2"] = 0;
-        cells["B3"] = 0;
-        cells["C1"] = 0;
-        cells["C2"] = 0;
-        cells["C3"] = 0;
-
     }
 
     void OnMouseDown()
     {
-            gameOver();
-            currentBox = getObjectClicked();
-            changeBoardPiece(currentBox);
-    }
-
-    /// <summary>
-    /// Method where is checked who wins.
-    /// </summary>
-    /// <returns>Void</returns>
-
-    void gameOver()
-    {
-        Debug.Log("A1 = " + cells["A1"]);
-        Debug.Log("A2 = " + cells["A2"]);
-        Debug.Log("A3 = " + cells["A3"]);
-        Debug.Log("B1 = " + cells["B1"]);
-        Debug.Log("B2 = " + cells["B2"]);
-        Debug.Log("B3 = " + cells["B3"]);
-        Debug.Log("C1 = " + cells["C1"]);
-        Debug.Log("C2 = " + cells["C2"]);
-        Debug.Log("C3 = " + cells["C3"]);
-
-        if (cells["A1"] == 1 && cells["A2"] == 1 && cells["A3"] == 1 ||
-            cells["A1"] == 1 && cells["B1"] == 1 && cells["C1"] == 1 ||
-            cells["A1"] == 1 && cells["B2"] == 1 && cells["C3"] == 1 ||
-            cells["A2"] == 1 && cells["B2"] == 1 && cells["C2"] == 1 ||
-            cells["A3"] == 1 && cells["B3"] == 1 && cells["C3"] == 1 ||
-            cells["A3"] == 1 && cells["B2"] == 1 && cells["C1"] == 1 ||
-            cells["B1"] == 1 && cells["B2"] == 1 && cells["B3"] == 1 ||
-            cells["C1"] == 1 && cells["C2"] == 1 && cells["C3"] == 1)
-        {
-            Debug.Log("Cross Wins");
-            Time.timeScale = 0;
-        }
-        else if (cells["A1"] == 2 && cells["A2"] == 2 && cells["A3"] == 2 ||
-          cells["A1"] == 2 && cells["B1"] == 2 && cells["C1"] == 2 ||
-          cells["A1"] == 2 && cells["B2"] == 2 && cells["C3"] == 2 ||
-          cells["A2"] == 2 && cells["B2"] == 2 && cells["C2"] == 2 ||
-          cells["A3"] == 2 && cells["B3"] == 2 && cells["C3"] == 2 ||
-          cells["A3"] == 2 && cells["B2"] == 2 && cells["C1"] == 2 ||
-          cells["B1"] == 2 && cells["B2"] == 2 && cells["B3"] == 2 ||
-          cells["C1"] == 2 && cells["C2"] == 2 && cells["C3"] == 2)
-        {
-            Debug.Log("Circle Wins");
-            Time.timeScale = 0;
-        }
+        GameObject camera = GameObject.Find("Camera");
+        BoardLogic boardLogic = camera.GetComponent<BoardLogic>();
+        currentBox = getObjectClicked();
+        changeBoardPiece(currentBox);
+        boardLogic.gameOver();
     }
 
     /// <summary>
@@ -116,20 +63,20 @@ public class PlayerAction : MonoBehaviour {
             Debug.Log("current box tag = " + currentBoxTag);
             SpriteRenderer currentBoxSpriteRenderer = currentBox.transform.gameObject.GetComponent<SpriteRenderer>();
 
-            if (cells[currentBoxTag] == 0)
+            if (boardLogic.cells[currentBoxTag] == 0)
             {
-                Debug.Log("cells[currentBoxTag] = " + cells[currentBoxTag]);
+                Debug.Log("cells[currentBoxTag] = " + boardLogic.cells[currentBoxTag]);
                 if (isCross)
                 {
                     currentBoxSpriteRenderer.sprite = crossSprite;
-                    cells[currentBoxTag] = 1;
-                    Debug.Log("cells[currentBoxTag] = " + cells[currentBoxTag]);
+                    boardLogic.cells[currentBoxTag] = 1;
+                    Debug.Log("cells[currentBoxTag] = " + boardLogic.cells[currentBoxTag]);
                 }
                 else
                 {
                     currentBoxSpriteRenderer.sprite = circleSprite;
-                    cells[currentBoxTag] = 2;
-                    Debug.Log("cells[currentBoxTag] = " + cells[currentBoxTag]);
+                    boardLogic.cells[currentBoxTag] = 2;
+                    Debug.Log("cells[currentBoxTag] = " + boardLogic.cells[currentBoxTag]);
                 }
 
                 //Must be changed only after click on an empty box.
